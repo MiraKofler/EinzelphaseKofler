af7078754ee6bad035447a386a0de7bef84acc86
Mira Kofler <mirako@edu.aau.at>
2021-03-10 19:30:05 +0100

    Refactoring

--- Einzelphase_Kofler/app/src/main/java/com/example/einzelphase_kofler/MainActivity.java
+++ Einzelphase_Kofler/app/src/main/java/com/example/einzelphase_kofler/MainActivity.java
@@ -22,59 +22,19 @@ public class MainActivity extends AppCompatActivity {
     public static TextView textViewCalculate;
     public Button buttonCalculate;
 
-
-    public static void main(String[] args) {
-
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         editTextMatrikelnummer = findViewById(R.id.edittext_matrikelnummer);
-        buttonSend = (Button) findViewById(R.id.button_send);
+        buttonSend = findViewById(R.id.button_send);
         textViewResult = findViewById(R.id.textview_result);
+
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Thread() {
-                    public void run() {
-                        String sentence = editTextMatrikelnummer.getText().toString();
-                        String modifiedSentence;
+                listenerButtonSend();
 
-                        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-                        try {
-                            Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
-
-                            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-
-                            BufferedReader inFromServer = new BufferedReader
-                                    (new InputStreamReader(clientSocket.getInputStream()));
-
-                            //sentence = editTextMatrikelnummer.getText().toString();
-
-                            outToServer.writeBytes(sentence + '\n');
-
-                            modifiedSentence = inFromServer.readLine();
-
-                            //System.out.println("FROM SERVER: " + modifiedSentence);
-                            //RUNSUITHREAT einen code schreoben der auch auf das user interface zugreifen kann und es ändern darf
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    textViewResult.setText(modifiedSentence);
-                                }
-                            });
-
-                            clientSocket.close();
-
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }.start();
             }
         });
 
@@ -84,52 +44,106 @@ public class MainActivity extends AppCompatActivity {
         buttonCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Thread() {
-                    public void run() {
-                        if (editTextMatrikelnummer.getText().toString().length() == 0) {
-                            editTextMatrikelnummer.setText(0);
-                        }
+                listenerButtonCalculate();
 
-                        int num = Integer.parseInt(editTextMatrikelnummer.getText().toString());
-                        int result = toArray(num);
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                textViewCalculate.setText(String.valueOf(result));
-                            }
-                        });
-
-                    }
-                }.start();
             }
         });
     }
 
+    private void listenerButtonSend(){
+        new Thread() {
+            public void run() {
+                String sentence = editTextMatrikelnummer.getText().toString();
+                try {
+                    Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+
+                    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+                    BufferedReader inFromServer = new BufferedReader
+                            (new InputStreamReader(clientSocket.getInputStream()));
+
+                    outToServer.writeBytes(sentence + '\n');
+
+                    String modifiedSentence = inFromServer.readLine();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textViewResult.setText(modifiedSentence);
+                        }
+                    });
+
+                    clientSocket.close();
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    private void listenerButtonCalculate() throws NumberFormatException{
+        try {
+            new Thread() {
+                public void run() {
+                    if (editTextMatrikelnummer.getText().toString().length() == 0) {
+                        editTextMatrikelnummer.setText(-1);
+                    }
+
+                    int num = Integer.parseInt(editTextMatrikelnummer.getText().toString());
+                    int result = toArray(num);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (result == -1) {
+                                textViewCalculate.setText(-1);
+                            } else {
+                                textViewCalculate.setText(String.valueOf(result));
+                            }
+                        }
+                    });
+
+                }
+            }.start();
+        }catch(NumberFormatException e){
+
+        }
+    }
+
     private int toArray(int num) {
-        //save the number in a Array
+        //save the number in an Array
         String buf = Integer.toString(num);
         int[] array = new int[buf.length()];
         for (int i = 0; i < buf.length(); i++) {
             array[i] = Character.getNumericValue(buf.charAt(i));
         }
-
-        //sort the array
         array = bubbleSort(array);
-
-
-        //turns array into an integer
-        int result = 0;
-        for (int a : array) {
-            result = 10 * result + a;
-        }
+        int result = finalProduct(array);
 
         return result;
     }
 
+    private int finalProduct(int[] array){
+        //turns array into an integer
+        int result = 0;
+        int primenum = 0;
+        for (int a : array) {
+            if(a == 2 || a == 3 || a == 5 || a == 7){
+                primenum = 10 * result + a;
+            }else {
+                result = 10 * result + a;
+            }
+        }
+
+        if(primenum > 10000000)     return -1;
+        else                        return result;
+    }
+
     private static int[] bubbleSort(int[] arr) {
         int n = arr.length;
-        int temp = 0;
+        int temp;
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < (n - i); j++) {
                 if (arr[j - 1] > arr[j]) {
@@ -137,19 +151,9 @@ public class MainActivity extends AppCompatActivity {
                     arr[j - 1] = arr[j];
                     arr[j] = temp;
                 }
-
             }
         }
         return arr;
     }
 
-
 }
-
-
-//therad starten
-//thread verbinden mit nachricht
-//nachricht darstellen
-//entweder in main warten mit getter und setter PROBLEM haben thread für netzwerk methode aber main thread warten wir sthene
-//BESSER im thread kann man mit RUNSUITHREAT einen code schreoben der auch auf das user interface zugreifen kann und es ändern darf
-//im netzwerk thread verbinden, mit antwort runuithreat(runnable) und darin auf die ui elemente zugreifen und ändern
